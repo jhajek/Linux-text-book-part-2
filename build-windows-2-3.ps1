c3b34361a42b2756224b010f0b8bcb3830b7bb86
Jeremy Hajek <hajek@iit.edu>
2016-03-10 14:54:19 -0600

    Updated the name of the file output from build script

--- build-windows-2-3.ps1
+++ build-windows-2-3.ps1
@@ -4,6 +4,6 @@ $STAMP=[Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"
 # PDF - #If you want to create a PDF, you’ll need to have LaTeX installed. (See MacTeX on OS X, MiKTeX on Windows, or
 # install the texlive package in linux.) Then do                
 ########################################################################################################################
-pandoc -V geometry:margin=1in -s -o ./output/pdf/Understanding-the-Technology-and-Philosophy-of-Linux-Part-II-$STAMP.pdf ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md 
+pandoc -V geometry:margin=1in -s -o ./output/pdf/Understanding-the-Technology-and-Philosophy-of-Linux-Part-II-2-3-$STAMP.pdf ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md 
 # http://stackoverflow.com/questions/19397100/adding-a-title-page-page-headers-and-footers-using-pandoc
 
